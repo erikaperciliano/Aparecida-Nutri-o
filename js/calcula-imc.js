e2c1cd1df1110b5550216525e1fc7d3b4e21a40e
Erika de Almeida Perciliano <erikaperciliano.developer@gmail.com>
2019-09-16 13:10:48 -0300

    acrescentando os valores dentro do objeto

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -1,19 +1,8 @@
-/*var titulo = document.querySelector(".titulo");
-titulo.addEventListener("click", function(){
-    titulo.textContent = "Mudei!";
-})*/
-//teste
-$(".titulo").click(function(){
-    $(".titulo").text("Mudei!");
-});
-
-//seleciona todas as trs do 1ª formulario
 var pacientes = document.querySelectorAll(".paciente");
 
 for (var i = 0; i < pacientes.length; i++) {
 
     var paciente = pacientes[i];
-  
   
     var tdPeso = paciente.querySelector(".info-peso");
     var peso = tdPeso.textContent;
@@ -21,10 +10,8 @@ for (var i = 0; i < pacientes.length; i++) {
     var tdAltura = paciente.querySelector('.info-altura');
     var altura = tdAltura.textContent;
 
- 
     var tdImc = paciente.querySelector(".info-imc");
-    console.log(tdImc);
-   // var imc = tdImc.textContent;
+
 
     var pesoEhValido = true;
     var alturaEhValida = true;
@@ -33,18 +20,16 @@ for (var i = 0; i < pacientes.length; i++) {
     if (peso <= 0 || peso >= 1000) {
         pesoEhValido = false;
         tdImc.textContent = "Peso inválido!";
-        //paciente.classList.add("paciente-invalido");//adiciona a classe paciente-invalido
     }
 
     if (altura <= 0 || altura >= 3.00) {
         alturaEhValida = false;
         tdImc.textContent = "Altura inválida!";
-        //paciente.classList.add("paciente-invalido");
     }
 
     if (alturaEhValida && pesoEhValido) {
         var imc = calculaImc(peso, altura);
-        tdImc.textContent = imc;//passa para o campo html o valor do calculo 
+        tdImc.textContent = imc;
         }
     }
 
--- js/form.js
+++ js/form.js
@@ -1,46 +1,50 @@
+//responsável por cuidar de todos os arquivos que tenham a ver com o formulário
+
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function(event){
     event.preventDefault();
     
-    var form = document.getElementById("form-adiciona");
-    var paciente = obtemPacienteDoFormulario(form);//retorna os valores dos campos do form
-    console.log(paciente);
- 
-    //cria uma nova tr e tds do paciente
+    var form = document.querySelector("#form-adiciona");
+    // Extrai as informacoes do paciente do form
+
+    var paciente = obtemPacienteDoFormulario(form);
+    
+    //cria os novos elementos
     var pacienteTr = document.createElement("tr");
     var nomeTd = document.createElement("td");
     var pesoTd = document.createElement("td");
     var alturaTd = document.createElement("td");
     var gorduraTd = document.createElement("td");
     var imcTd = document.createElement("td");
+    
+    //atribui as variáveis criadas acima, os valores do objeto paciente(que se encontra dentro da função)
+    nomeTd.textContent = paciente.nome;
+    pesoTd.textContent = paciente.peso;
+    alturaTd.textContent = paciente.altura;
+    gorduraTd.textContent = paciente.gordura;
+    imcTd.textContent = paciente.imc;
 
-    //os novos campos recebem o valor digitado
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-    imcTd.textContent = calculaImc(peso, altura);//chama a função presente no calcula-imc.js
-
-    //adiciona as novas tds dentro da tr
+    //adiciona todas as tds dentro da tr
     pacienteTr.appendChild(nomeTd);
     pacienteTr.appendChild(pesoTd);
     pacienteTr.appendChild(alturaTd);
     pacienteTr.appendChild(gorduraTd);
     pacienteTr.appendChild(imcTd);
 
-    //adiciona a nova tr dentro da tabela já existente
-    var tabela = document.querySelector("#tabela-pacientes"); 
-    tabela.appendChild(pacienteTr);   
+    //acrescenta a tr c/ as tds dentro da tabela já existente
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
 });
-//pega todos os valores e extrai para variáveis
-    function obtemPacienteDoFormulario(form) {
-        var paciente = {
-            nome: form.nome.value,
-            peso: form.peso.value,
-            altura: form.altura.value,
-            gordura: form.gordura.value,
-            imc: calculaImc(form.peso.value, form.altura.value)//pega o valor do peso e da altura e realiza o cálculo
-        }
-        return paciente; // a função retorna o obj paciente c/ seus valores
+
+function obtemPacienteDoFormulario(form){
+    //Atribui os valores do form p/ as novas variáveis criadas
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc:calculaImc(form.peso.value, form.altura.value)
     }
     
+    return paciente;
+}
